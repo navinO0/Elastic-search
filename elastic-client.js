530f040bff7eb4c83f5246c1f984ffc28e3c47d1
navinO0 <6y.navin@gmail.com>
2023-06-09 17:54:16 +0530

    first commit

--- elastic-client.js
+++ elastic-client.js
@@ -3,25 +3,6 @@ const { Client } = require("@elastic/elasticsearch");
 // const elasticsearch = require('elasticsearch');
 
 
-// const { Client } = require('@elastic/elasticsearch')
-// const elasticClient = new Client({ node: 'http://localhost:9200' })
-
-// const elasticClient = new Client({
-//   hosts: [ 'http://localhost:9200']
-//   });
-
-// elasticClient.ping({
-//     requestTimeout: 30000,
-//     }, function(error) {
-//     if (error) {
-//     console.error('Cannot connect to Elasticsearch.');
-//     } else {
-//     console.log('Connected to Elasticsearch was successful!');
-//     }
-//     });
-
-
-// require("dotenv").config({ path: ".elastic.env" });
 
 const elasticClient = new Client({
   cloud: {
@@ -34,7 +15,7 @@ const elasticClient = new Client({
 });
 
 
-
+// for creating index in elastic database
 
 // const createIndex = async (indexName) => {
 //   await elasticClient.indices.create({ index: indexName });
@@ -44,55 +25,7 @@ const elasticClient = new Client({
 // createIndex("store");
 
 
-const products = [
-    {
-      name: 'Smart Scale',
-      description: 'Track your weight and health metrics with this smart scale.',
-      price: 49.99
-    },
-    {
-      name: 'Car Dash Cam',
-      description: 'Capture your journey with this high-definition dash cam.',
-      price: 89.99
-    },
-    {
-      name: 'Noise-Canceling Headphones',
-      description: 'Premium headphones that block out ambient noise.',
-      price: 299.99
-    },
-    {
-      name: 'Bluetooth Speaker',
-      description: 'Portable speaker with wireless Bluetooth connectivity.',
-      price: 59.99
-    },
-    {
-      name: 'Robot Vacuum Cleaner',
-      description: 'Effortlessly clean your home with this smart vacuum cleaner.',
-      price: 349.99
-    },
-    {
-      name: 'Portable Power Bank',
-      description: 'Compact power bank for charging your devices on the go.',
-      price: 39.99
-    }]
 
-let timeInterval = null
-let counter = 0
-// products.forEach((eachProduct) => {
-  
-
-//   timeInterval = setTimeout(async ()=>{
-//     const result = await elasticClient.index({
-//       index: "store",
-//       document: eachProduct,
-//     });
-//     counter += 1
-//     console.log(eachProduct)
-
-//   },3000)
-
-  
-// })
 
 
 
